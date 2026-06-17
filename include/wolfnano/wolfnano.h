2096053364d19420cfee2655189ad54e17601c71
aidan garske <aidan@wolfssl.com>
2026-06-17 14:16:36 -0700

    [crypto] add TLS 1.3 key-schedule core over wc_* seam

---
+++ include/wolfnano/wolfnano.h
@@ -0,0 +1,40 @@
+/* wolfnano.h
+ *
+ * Copyright (C) 2026 wolfSSL Inc.
+ *
+ * This file is part of wolfNanoTLS.
+ *
+ * wolfNanoTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfNanoTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * wolfNanoTLS public API surface: shared types and return codes.
+ */
+
+#ifndef WOLFNANOTLS_H
+#define WOLFNANOTLS_H
+
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/wolfcrypt/types.h>
+
+#define WOLFNANOTLS_API
+#define WOLFNANOTLS_LOCAL
+
+/* Return codes: success is 0, errors are negative. */
+#define WOLFNANOTLS_SUCCESS         0
+#define WOLFNANOTLS_E_INVALID_ARG (-1)
+#define WOLFNANOTLS_E_CRYPTO      (-2)
+#define WOLFNANOTLS_E_UNSUPPORTED (-3)
+
+#endif /* WOLFNANOTLS_H */
